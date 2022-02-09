0bf0a55ea06a869d3f2ae6b0dd99b644ee22762a
Sancho Jr <sanchojunior.ce@gmail.com>
2022-02-09 17:19:33 -0300

    Adjust Controller Categories

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -7,11 +7,7 @@ module Admin::V1
         def create
             @category = Category.new
             @category.attributes = category_params
-
-            @category.save!
-            render :show
-            rescue
-                render json: { errors: { fields: @category.errors.messages }}, status: :unprocessable_entity
+            save_category!
         end    
 
         private
@@ -19,6 +15,13 @@ module Admin::V1
         def category_params
             return {} unless params.has_key?(:category)
             params.require(:category).permit(:name)
-        end    
+        end
+
+        def save_category!
+            @category.save!
+            render :show
+        rescue
+            render_error(fields: @category.errors.messages)
+        end
     end    
 end    
